aa2c456b0fd2f8240388151f2bab3370e6fcb286
Ted Robinson <tfqbtd@aol.com>
2018-05-24 13:49:11 -0500

    added routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
+import { CreateComponent } from './create/create.component';
+import { EditComponent } from './edit/edit.component';
+import { appRoutes } from './routerConfig';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    CreateComponent,
+    EditComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/routerConfig.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import { CreateComponent } from './create/create.component';
+import { EditComponent } from './edit/edit.component';
+import { IndexComponent } from './index/index.component';
+
+export const appRoutes: Routes = [
+  { path: 'create', component: CreateComponent  },
+  { path: 'edit/:id', component: EditComponent },
+  { path: 'index', component: IndexComponent}
+];
