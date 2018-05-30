32561850ecf2cf7f7ee504c108fb9c4cd54c86ca
Ted Robinson <tfqbtd@aol.com>
2018-05-29 20:25:25 -0500

    add prod url ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
+  // tslint:disable-next-line:max-line-length
+  'url': `postgres://ddpdvlujtbflwv:4bfec4912dbaf8969f9bd4fe6b51936f34781e2a2edd713257c12ddc9d6dcff3@ec2-54-243-235-153.compute-1.amazonaws.com:5432/dc79kjvbe6a50c`,
+  'dialect': 'postgres',
+  'ssl': true,
+  'operatorsAliases': false,
   production: true
 };
