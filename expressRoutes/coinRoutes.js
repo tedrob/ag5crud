01c81c3084de1bc5530a7373dee52ce15f5a297f
Ted Robinson <tfqbtd@aol.com>
2018-05-31 05:12:42 -0500

    add client

--- expressRoutes/coinRoutes.js
+++ expressRoutes/coinRoutes.js
@@ -12,6 +12,15 @@ let sequelize; //
 // ---
 console.log('routes', process.env.NODE_ENV);
 
+const { Client } = require('pg');
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+client.connect();
+console.log('client',client.DATABASE_URL);
+
 if (process.env.NODE_ENV) { // production
   console.log('routes-prod');
   sequelize = new Sequelize(config.url, {
