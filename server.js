a25b5827eac4a9416bda35ec2b31d26dec7a902d
Ted Robinson <tfqbtd@aol.com>
2018-06-05 07:07:17 -0500

    reduce server

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const express = require('express'), //
       bodyParser = require('body-parser'),
       favicon = require('serve-favicon'),
       coinRoutes = require('./expressRoutes/coinRoutes'),
-      // db = require('./model/coin'),
+      db = require('./model/coin'),
       config = require('./config/config.json'),
       PORT = process.env.PORT || 8080,
       Sequelize = require('sequelize'),
@@ -23,7 +23,8 @@ const express = require('express'), //
           idle: 10000
         },
       });
-const protectCfg = {
+
+/* const protectCfg = {
   production: process.env.NODE_ENV === 'production', // if production is false, detailed error messages are exposed to the client
   clientRetrySecs: 1, // Client-Retry header, in seconds (0 to disable) [default 1]
   sampleInterval: 5, // sample rate, milliseconds [default 5]
@@ -33,22 +34,15 @@ const protectCfg = {
   errorPropagationMode: false // dictate behavior: take over the response
   // or propagate an error to the framework [default false]
 };
-const protect = require('overload-protection')('express', protectCfg);
-
-// const cors = require('cors');
-// const PORT = process.env.PORT || 8080;
-// configuration ===========================//
-// const pg = require('pg'); //
-const db = require('./model/coin');
-
+const protect = require('overload-protection')('express', protectCfg); */
 // Sets up the Express app to handle data parsing
 
-app.use(protect);
+// app.use(protect);
 app.use(cors());
 app.options('*', cors());
 app.use(bodyParser.json());
 
-function ignoreFavicon(req, res, next) {
+/* function ignoreFavicon(req, res, next) {
   if (req.originalUrl === './public/favicon.ico') {
     res.status(204).json({
       nope: true
@@ -59,11 +53,10 @@ function ignoreFavicon(req, res, next) {
   }
 };
 
-app.use(ignoreFavicon);
+app.use(ignoreFavicon); */
 
-app.use(express.static('public')); //
-app.use(favicon(path.join(__dirname, './public/favicon.ico')));
-
+// app.use(express.static('public')); //
+// app.use(favicon(path.join(__dirname, './public/favicon.ico')));
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(bodyParser.text());
@@ -75,9 +68,9 @@ if ('production' === app.get('env')) {
   const env = app.get('env')
   console.log('server server-production=',env);
   app.use(express.static(path.join(__dirname, './dist')));
-  app.use(express.static(path.join(__dirname, './public')));
-  app.use(favicon(path.join(__dirname, './public/favicon.ico')));
-  app.use(favicon(path.join(__dirname, './dist/favicon.ico')));
+  // app.use(express.static(path.join(__dirname, './public')));
+  // app.use(favicon(path.join(__dirname, './public/favicon.ico')));
+  // app.use(favicon(path.join(__dirname, './dist/favicon.ico')));
   // app.use(ignoreFavicon);
   app.get('/*', (req, res, next) => {
     res.sendFile(path.join(__dirname, './dist/index.html'));
@@ -89,10 +82,10 @@ else {
   console.log('server server-development=',env);
   console.log('dirname=', __dirname);
   app.use(express.static(path.join(__dirname, './dist')));
-  app.use(express.static(path.join(__dirname, './public')));
-  app.use(favicon(path.join(__dirname, './public//favicon.ico')));
-  app.use(favicon(path.join(__dirname, './dist/favicon.ico')));
-  app.use(ignoreFavicon);
+  // app.use(express.static(path.join(__dirname, './public')));
+  // app.use(favicon(path.join(__dirname, './public//favicon.ico')));
+  // app.use(favicon(path.join(__dirname, './dist/favicon.ico')));
+  // app.use(ignoreFavicon);
   app.get('/*', (req, res, next) => {
     res.sendFile(path.join(__dirname, './dist/index.html'));
     next;
